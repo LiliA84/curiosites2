c91f666d905fda85dc19bf3559e0bedd8542de82
lili <lili2secstr@gmail.com>
2018-06-16 12:08:35 +0000

    tableau file added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @name = 'Happy Cat'
+    @surname = 'miaou'
+    @description = 'Hungry cat'
+    @comment = 'no comment'
+    
+    @curiosities = [
+      'pelote de laine',
+      'tapis de souris',
+      'blablabla']
   end
 end
+
---
+++ app/models/tableau.rb
@@ -0,0 +1,13 @@
+class Tableau
+    def chaine1
+        'chaine1'
+    end
+    
+    def chaine2
+        'chaine2'
+    end
+    
+    def chaine3
+        'chaine3'
+    end
+end
